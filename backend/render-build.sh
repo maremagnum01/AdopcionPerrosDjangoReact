76029b6e76d0bd76ed314f8a6e02a2ee3bb59203
Maremagnum01 <eze.di.lallo@gmail.com>
2025-06-24 19:18:46 -0300

    otro camnbio render

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,5 +1,11 @@
+#Script para render, dependencias y migraciones
 #!/usr/bin/env bash
 
-# ¡Este script se ejecuta después del build!
+# Instala dependencias
+pip install -r requirements.txt
+
+# Aplica migraciones de base de datos
 python manage.py migrate
+
+# Junta archivos estáticos
 python manage.py collectstatic --noinput
